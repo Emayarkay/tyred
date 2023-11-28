492cf5461a9dc93ab377c6dbe078927475d4b30f
Robbie Bullough <robbie.bullough@hotmail.co.uk>
2023-11-28 12:07:28 +0000

    Created component model and migrated to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_115637) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_120650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_115637) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_bikes_on_user_id"
+  end
+
+  create_table "components", force: :cascade do |t|
+    t.string "name"
+    t.time "time_until_check"
+    t.float "distance_until_check"
+    t.text "check_advice"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
