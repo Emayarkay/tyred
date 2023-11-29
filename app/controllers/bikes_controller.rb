fae9fbf27c8828f93da3587c0c74ae27a36ded8e
Robbie Bullough <robbie.bullough@hotmail.co.uk>
2023-11-29 12:07:07 +0000

    Altered the new bike method (removed unnecessary line of code) and added a set_bike private method to make the bike controller DRY

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,11 +1,11 @@
 class BikesController < ApplicationController
+  before_action :set_bike, only: %i[edit update destroy]
+
   def index
     @bikes = current_user.bikes
   end
 
   def new
-    # Linking rails link helper at bottom of page to show page for user's first bike - may need to change if my garage concept changes
-    @user_first_bike = current_user.bikes.first
     @bike = Bike.new
   end
 
@@ -20,22 +20,23 @@ class BikesController < ApplicationController
   end
 
   def edit
-    @bike = Bike.find(params[:id])
   end
 
   def update
-    @bike = Bike.find(params[:id])
     @bike.update(bike_params)
     redirect_to bikes_path
   end
 
   def destroy
-    @bike = Bike.find(params[:id])
     @bike.destroy!
     redirect_to bikes_path
   end
 
   private
+
+  def set_bike
+    @bike = Bike.find(params[:id])
+  end
 
   def bike_params
     params.require(:bike).permit(:make, :model)
