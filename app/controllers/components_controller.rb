89854d8851fd5e038c3642271eb8bcfb38afa950
Robbie Bullough <robbie.bullough@hotmail.co.uk>
2023-11-29 16:21:44 +0000

    Made progress with components controller and bike components comntroller (Not finished)

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,3 +1,20 @@
 class ComponentsController < ApplicationController
-  def 
+
+  def create
+    @component = Component.new(component_params)
+    @bike = Bike.find(params[:bike_id])
+    if @component.save!
+      @bike_component = BikeComponent.new(bike: @bike, component: @component, date_added: DateTime.now, distance_travelled: 0)
+      @bike_component.save!
+      redirect_to bikes_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def component_params
+    params.require(:component).permit(:name, :time_until_check, :distance_until_check, :check_advice)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new show edit update create destroy]
   resources :bikes do
-    resources :bike_components, only: %i[show edit update destroy]
+    resources :bike_components, except: :index
+    resources :components, only: :create
   end
-  resources :components, only: %i[new create]
   resources :checks, only: :update
 end
