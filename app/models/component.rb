4b717a4df930562e1708447d1fb9615c3e2defce
Jonny Shipton <jonathanshipton1@gmail.com>
2023-11-30 15:32:29 +0000

    Edited seeds file and bike index page

--- app/models/component.rb
+++ app/models/component.rb
@@ -2,4 +2,7 @@ class Component < ApplicationRecord
   has_many :bike_components
   has_many :bikes, through: :bike_components
   validates :name, :time_until_check, :distance_until_check, presence: true
+
+  has_one_attached :icon
+  has_one_attached :check_img
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post '/strava/setup', to: 'strava#setup'
   get '/redirect', to: 'strava#redirect'
 
-  root to: "pages#home"
+  root to: "application#home"
   # root to: 'bikes#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
