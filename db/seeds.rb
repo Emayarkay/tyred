efb848ff271d0e1de69b9d8fa55e528735ad9a4c
Mark Watkinson <markjwatkinson@gmail.com>
2023-11-30 11:29:57 +0000

    add controller definition

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
+ 
 User.destroy_all
 Bike.destroy_all
 Component.destroy_all
