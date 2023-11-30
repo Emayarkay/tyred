063204d557aedb5551b0a35f1929a19ca29f6218
Lucien George <luciengeorge95@gmail.com>
2023-11-30 16:21:44 +0000

    Remove users#destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,4 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to root_path
-  end
 end
