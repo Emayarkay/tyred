18b351a312793bb3ee8a61b5b177bec7fd8e7095
Elliot Griffin <elliotg283@gmail.com>
2023-12-01 12:47:04 +0000

    it works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = current_user
+    @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user)
+    redirect_to @user
   end
 
   def destroy
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :profile_image)
   end
 
 end
