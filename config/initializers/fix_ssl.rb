b72de86baa633966e0d876fa55b153fd7be079fd
Roel de Jong <Roel4811@hotmail.com>
2023-12-06 16:16:22 +0000

    only monkeypatch on prod

--- config/initializers/fix_ssl.rb
+++ config/initializers/fix_ssl.rb
@@ -1,15 +1,17 @@
 require 'open-uri'
 require 'net/https'
 
-module Net
-  class HTTP
-    class_eval do
-      alias_method :original_use_ssl=, :use_ssl=
+if Rails.env == 'production'
+  module Net
+    class HTTP
+      class_eval do
+        alias_method :original_use_ssl=, :use_ssl=
 
-      def use_ssl=(flag)
-        self.ca_path = Rails.root.join('/usr/lib/ssl/certs/ca-certificates.crt').to_s
-        self.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        self.original_use_ssl = flag
+        def use_ssl=(flag)
+          self.ca_file = Rails.root.join('/usr/lib/ssl/certs/ca-certificates.crt').to_s
+          self.verify_mode = OpenSSL::SSL::VERIFY_PEER
+          self.original_use_ssl = flag
+        end
       end
     end
   end
